2e72c654a2844ffcd37f552168e92c8b7b6eda8a
24 <39792363+1302557841@users.noreply.github.com>
2024-01-27 22:55:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,4 +20,4 @@
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
 echo 'src-git smartdns https://github.com/pymumu/smartdns' >>feeds.conf.default
-echo 'luci-app-smartdns https://github.com/pymumu/luci-app-smartdns' >>feeds.conf.default
+echo 'src-git luci-app-smartdns https://github.com/pymumu/luci-app-smartdns' >>feeds.conf.default
