2d5daa070528c57772222547d4cc200590a2ef09
24 <39792363+1302557841@users.noreply.github.com>
2024-01-27 22:12:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,6 +20,7 @@ echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-inf
 # passwall
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
+sed -i -e '/^#/d' feeds.conf.default
 sed -i '$a src-git smartdns https://github.com/pymumu/smartdns' feeds.conf.default
 sed -i '$a src-git luci-app-smartdns https://github.com/pymumu/luci-app-smartdns' feeds.conf.default
 
