8a12c0a71deb338e977010709d6fe88e03c3b8df
24 <39792363+1302557841@users.noreply.github.com>
2024-01-28 11:12:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # luci-theme-infinityfreedom
-echo 'src-git smartdns https://github.com/pymumu/smartdns' >> feeds.conf.default
-echo 'src-git luciappsmartdns https://github.com/pymumu/luci-app-smartdns' >> feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git smartdns https://github.com/pymumu/smartdns' >> feeds.conf.default
+# echo 'src-git luciappsmartdns https://github.com/pymumu/luci-app-smartdns' >> feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git onliner https://github.com/rufengsuixing/luci-app-onliner' >> feeds.conf.default
 # echo 'src-git luciappserverchan https://github.com/tty228/luci-app-serverchan' >> feeds.conf.default
